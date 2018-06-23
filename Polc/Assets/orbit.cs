361372c53f42aecccf9d02ac194d5fb6d4d63936
Radosław Smutek <radsmu45@gmail.com>
2018-06-23 16:07:55 +0200

    Polcraft

---
+++ Polc/Assets/orbit.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class orbit : MonoBehaviour {
+
+	public float speed = 0.1f;
+	
+	void Update () {
+		this.transform.RotateAround(Vector3.zero, 
+			Vector3.right, speed);
+	}
+}
