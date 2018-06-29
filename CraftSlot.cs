e1e5d37fb1dec7ed308b39eb86f8631ac9d69b04
maaarcia96 <40038722+maaarcia96@users.noreply.github.com>
2018-06-29 23:56:20 +0200

    Add files via upload

---
+++ CraftSlot.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+public class CraftSlot : MonoBehaviour, IPointerClickHandler {
+
+    Crafting craftingScript;
+    public Item myItem;
+    Image myIcon;
+	// Use this for initialization
+	void Start () {
+        if (myItem == null)
+        {
+            Destroy(gameObject);
+        }
+        craftingScript = GameObject.FindObjectOfType<Crafting>();
+        myIcon.sprite = myItem.itemIcon;
+	}
+	public void OnPointerClick(PointerEventData eventData)
+    {
+        craftingScript.CraftItem(myItem);
+    }
+}
---
+++ Crafting.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Crafting : MonoBehaviour {
+
+    public ItemDatabase dataBase;
+    public Inventory inventory;
+    public GameObject craft;
+    public GameObject slot;
+
+    void Start()
+    {
+        GenSlots();
+    }
+    void GenSlots()
+    {
+        for (int i=0; i<dataBase.dataBaseItems.Count; i++)
+        {
+            Item currentItem = dataBase.dataBaseItems[i];
+            if (currentItem.isCraftable)
+            {
+                GameObject go = Instantiate(slot, craft.transform.position, Quaternion.identity);
+                go.transform.SetParent(craft.transform);
+                go.GetComponent<Slot>().myItem = currentItem;
+            }
+        }
+    }
+    public void CraftItem(Item itemToCraft)
+    {
+        if (itemToCraft.isCraftable)
+        {
+            if (CanCraft(itemToCraft))
+            {
+                Add(itemToCraft);
+            }
+            else
+            {
+                print("Nie można stworzyć");
+            }
+        }
+        else
+        {
+            return;
+        }
+    }
+    bool CanCraft(Item itemToLookUp)
+    {
+        for (int i = 0; i< itemToLookUp.craftItems.Count; i++){
+            Item currentItem = itemToLookUp.craftItems[i];
+            int currentAmount = itemToLookUp.craftAmount[i];
+            /*if (!inventory.HasItem(currentItem.itemName, currentAmount))
+            {
+                return false;
+            }*/
+        }
+        return true;
+    }
+    void Add(Item itemToAdd)
+    {
+        inventory.AddItem(itemToAdd, itemToAdd.makesHowMany);
+        Remove(itemToAdd);
+    }
+    void Remove(Item itemToRemove)
+    {
+        for (int i=0; i< itemToRemove.craftItems.Count; i++)
+        {
+            Item currentItem = itemToRemove.craftItems[i];
+            int currentAmount = itemToRemove.craftAmount[i];
+            inventory.RemoveItem(currentItem, currentAmount);
+
+        }
+    }
+    
+}
